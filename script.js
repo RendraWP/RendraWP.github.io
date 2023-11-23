fb5c1e207cc710c598fbe8675e6b69b1f3095e54
RendraWP <150310786+RendraWP@users.noreply.github.com>
2023-11-23 07:59:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx bx-x');
+    menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
 
-/*==================== scoll sections avtive link ====================*/
+/*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -15,7 +15,7 @@ window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height =sec.offsetHeight;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
@@ -23,27 +23,37 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        }; 
+        };
     });
-/*==================== sticky navbar ====================*/ 
-let header = document.querySelector('header');
+    /*==================== stciky navbar ====================*/
+    let header = document.querySelector('header');
 
-header.classList.toggle('sticky', window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-menuIcon.classList.remove('bx bx-x');
-navbar.classList.remove('active');
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 
-/*==================== sticky navbar ====================*/ 
-scrollReveal({ 
-    reset: true,
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    // reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
 });
 
-scrollReveal().reveal('.home-contact, .heading', { origin: 'top' });
-scrollReveal().reveal('.home-img, .services-container, .portofolio-box, .contact form', { origin:
-'bottom' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*==================== typed js ====================*/
+const typed = new Typed ('.multiple-text', {
+    strings: ['Pelajar', 'Desainer Web', 'Blogger',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
